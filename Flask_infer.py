f5d05162f387c3fc8d923568a5ad17db51bc96c2
abbasshujah <syed.shujah.abbas@gmail.com>
2020-04-26 09:56:13 -0700

    first commit to help run docker from jenkins

---
+++ Flask_infer.py
@@ -0,0 +1,77 @@
+import sys
+import os
+import shutil
+import time
+import traceback
+
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+from sklearn.externals import joblib
+
+
+clf = None
+
+app = Flask(__name__)
+
+# inputs
+training_data = 'data/titanic.csv'
+include = ['Age', 'Sex', 'Embarked', 'Survived']
+dependent_variable = 'Survived'
+
+model_directory = 'model'
+model_file_name = f'./simple_rand_forest.pkl'
+model_columns_file_name = f'{model_directory}/model_columns.pkl'
+
+first_line = "jk ____"
+second_line = "this is second"
+
+
+
+@app.route('/')
+def test():
+    return render_template('index.html', first_line = first_line, second_line = second_line)
+    #return render_page
+    
+
+
+@app.route('/predict', methods=['POST']) # Create http://host:port/predict POST end point
+def predict():
+    if clf:
+        try:
+            json_ = request.json #capture the json from POST
+            query = pd.get_dummies(pd.DataFrame(json_))
+            #query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(clf.predict(query))
+                        
+
+            return jsonify({'prediction': [int(x) for x in prediction]})
+
+        except Exception as e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print('train first')
+        return 'no model here'
+
+
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        port = 80
+
+    try:
+        clf = joblib.load(model_file_name)
+        print('model loaded')
+        
+
+    except Exception as e:
+        print("making sure")
+        print(e)
+        print('No model here')
+        print('Train first')
+        clf = None
+
+    app.run(host='0.0.0.0', port=port, debug=False)
